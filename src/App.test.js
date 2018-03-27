3d815e91fd9994f9abf7cd81c766b2e1bc449321
panacholn <panacholn@thinknet.co.th>
2018-03-27 12:32:50 +0700

    instruction

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,29 @@ const repng = require('repng')
 
 const Component = ({char}) => (<span style={{ fontSize: 300, background: 'white' }}>{char}</span>)
 
+/*
+  ----------------------------------------------------------------------------------------------------------------
+    Before run test change index.js of repng libs
+  ----------------------------------------------------------------------------------------------------------------
+
+  const markup = renderToStaticMarkup(h(Root, props))
+
+  const html = `
+    <!DOCTYPE html>
+      <html lang="th">
+      <head>
+          <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <meta http-equiv="X-UA-Compatible" content="ie=edge">
+          <title>Document</title>
+      </head>
+      <body>
+        ${markup}
+      </body>
+    </html>
+  ` 
+*/
+
 it('renders without crashing', () => {
   const options = {
     props: {
